3ec545850fdb34bc8cee4cbf67df9053462de1bc
九郎丸寛仁 <maru960a@gmail.com>
2025-12-14 18:36:07 +0900

    gaの導入

--- backend/src/app/layout.tsx
+++ backend/src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "チャレッタ",
   description: "あなたの韓国語学習をサポートするオールインワンアプリ。",
 };
+
+const GA_MEASUREMENT_ID = "G-MCFXSJ6S1C";
 
 export default function RootLayout({
   children,
@@ -13,6 +16,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
